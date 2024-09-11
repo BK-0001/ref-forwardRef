36671776608e97c0c748a9b8ebb5afb4952178a6
Bart Kang <bart.kang.0719@gmail.com>
2024-09-11 09:06:57 -0700

    demo when not to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0);
+
+  const countRef = useRef<number>(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prevState) => prevState + 1)}>
+        useState
+      </button>
+
+      <br />
+
+      <h1>{countRef.current}</h1>
+      <button
+        onClick={() => {
+          countRef.current += 1;
+          alert(countRef.current);
+        }}
+      >
+        useRef
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
